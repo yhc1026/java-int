a48874ac4935498fba1584c51ee54b9d1b3f9fa1
yhc1026 <478946668@qq.com>
2024-07-12 21:41:24 +0800

    2024/7/12

---
+++ JDK18/course.java
@@ -0,0 +1,5 @@
+public class course {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ JDK18/final_test.java
@@ -0,0 +1,5 @@
+public class final_test {
+    public static void main(String[] args) {
+        System.out.println("final test");
+    }
+}
