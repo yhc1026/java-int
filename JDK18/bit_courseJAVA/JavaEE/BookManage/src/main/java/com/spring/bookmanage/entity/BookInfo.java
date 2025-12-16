774e2411e52c48e631161aa1888fca19663e6d88
yhc1026 <478946668@qq.com>
2025-12-16 17:39:24 +0800

    2025/12/16

--- JDK18/bit_courseJAVA/JavaEE/BookManage/src/main/java/com/spring/bookmanage/entity/BookInfo.java
+++ JDK18/bit_courseJAVA/JavaEE/BookManage/src/main/java/com/spring/bookmanage/entity/BookInfo.java
@@ -3,14 +3,18 @@ package com.spring.bookmanage.entity;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class BookInfo {
-    private Integer bookId;
+    private Integer id;
     private String bookName;
     private String author;
-    private Integer num;
+    private Integer count;
     private float price;
     private String publish;
     private Integer status;
     private String statusCN;
+    private Date createTime;
+    private Date updateTime;
 }
---
+++ JDK18/bit_courseJAVA/JavaEE/BookManage/src/main/java/com/spring/bookmanage/entity/UserInfo.java
@@ -0,0 +1,17 @@
+package com.spring.bookmanage.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Data
+public class UserInfo {
+
+    private Integer id;
+    private String userName;
+    private String password;
+    private Integer deleteFlag;
+    private Date createTime;
+    private Date updateTime;
+}
